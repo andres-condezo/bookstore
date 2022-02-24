5a45585a46abd3e0406199f53d3665f499f6abb8
andres-condezo <ra.condezomonge@gmail.com>
2022-02-24 16:25:04 -0500

    Fix(Categories): [Fix status code for underconstruction]

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -2,7 +2,11 @@ import './Categories.css';
 
 const Categories = () => (
   <section className="categories-container">
-    <span>404</span>
+    <span>
+      5
+      <span>0</span>
+      3
+    </span>
     <h1>Under construction</h1>
   </section>
 );
