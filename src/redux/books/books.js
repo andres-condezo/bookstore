6e58519b8d4de8bff98ab75603c1ce06a0e2443b
andres-condezo <ra.condezomonge@gmail.com>
2022-02-25 14:47:52 -0500

    Fix(redux/): [Add reducers]

---
+++ src/redux/books/books.js
@@ -0,0 +1,31 @@
+// CONST
+const ADD_BOOK = 'bookStore/books/ADD_BOOK';
+const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
+const initialState = [];
+
+// Reducer
+const booksReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_BOOK:
+      return [...state, action.payload];
+
+    case REMOVE_BOOK:
+      return state.filter((book) => book.id !== action.id);
+
+    default:
+      return state;
+  }
+};
+
+// Action Creators
+export const addBook = (payload) => ({
+  type: ADD_BOOK,
+  payload,
+});
+
+export const removeBook = (id) => ({
+  type: REMOVE_BOOK,
+  id,
+});
+
+export default booksReducer;
