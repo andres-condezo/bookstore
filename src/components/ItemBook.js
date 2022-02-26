f85c93063ac2007751a754ae6b88bb5fb8730029
andres-condezo <ra.condezomonge@gmail.com>
2022-02-25 23:00:40 -0500

    Feat(components/ItemBook.js): [Update the handleRemoveBook function to delete a book from the API]

--- src/components/ItemBook.js
+++ src/components/ItemBook.js
@@ -3,7 +3,7 @@ import { useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
 import './ItemBook.css';
 import progress from '../img/progress.png';
-import { removeBook } from '../redux/books/books';
+import { deleteBook } from '../redux/books/books';
 
 const ItemBook = (props) => {
   const dispatch = useDispatch();
@@ -13,7 +13,7 @@ const ItemBook = (props) => {
   } = props;
 
   const handleRemoveBook = () => {
-    dispatch(removeBook(id));
+    dispatch(deleteBook(id));
   };
 
   return (
